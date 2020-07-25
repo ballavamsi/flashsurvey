dc97b3ebd3c7be123e2f2a9658f6d06e2882f889
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2020-07-25 13:01:11 +0530

    Survey initial version

--- src/app/create-survey/create-survey.component.ts
+++ src/app/create-survey/create-survey.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, FormControl, FormArray } from '@angular/forms';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { SurveyService } from '../services/survey/survey.service';
+import { OverlayService } from '../overlay/overlay.module';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
+import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'app-create-survey',
@@ -10,7 +15,15 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 export class CreateSurveyComponent implements OnInit {
 
   fg: FormGroup;
-  constructor(private _formBuilder: FormBuilder) {
+  questionOptions = [];
+  questionTypes = [];
+  newSurveyViewModel: SurveyModel;
+  constructor(private _formBuilder: FormBuilder,
+    private _surveyService : SurveyService,
+    private _overlayService: OverlayService,
+    private _snackBar: MatSnackBar,
+    private _router: Router,
+    public dialog: MatDialog) {
     this.fg = this._formBuilder.group({
       welcomeMessage: this._formBuilder.control('Welcome to the survey', [Validators.required]),
       welcomeImage: this._formBuilder.control(''),
@@ -30,34 +43,82 @@ export class CreateSurveyComponent implements OnInit {
     }
   }
   onSubmit() {
-    console.log(this.fg.value);
+    let data = this.fg.value;
+    let modifiedData = this.modifyBody(data);
+
+    this._surveyService.addSurvey(modifiedData).subscribe(
+      result => {
+        const returnData: SurveyModel = result;
+        this.newSurveyViewModel = returnData;
+        this._overlayService.hide();
+        this.openDialog('Survey Created successfully', 'Click on the link to copy', this.generateLink(returnData.surveyGuid), true);
+      },
+      error => {
+        this.openDismiss('Failed to create survey, please try again', 'Close');
+        this._overlayService.hide();
+      });
+  }
+
+  modifyBody(data: any){
+    let survey = new SurveyModel();
+    survey.welcometitle = data['welcomeMessage'];
+    survey.welcomeimage = data['welcomeImage'];
+    survey.emailidrequired = data['emailIdRequired'] ? 1 : 0;
+    survey.endtitle = data['endMessage'];
+    survey.allowduplicate = 0; //default 0
+    survey.enddate = new Date((new Date().getDate()) + 365).toISOString(); //set next year date default
+    survey.surveyQuestions = [];
+
+    data['questions'].forEach((element,index)  => {
+      let eachquestion = new SurveyQuestionsModel();
+      eachquestion.surveyQuestionId = 0;
+      eachquestion.questionDisplayOrder = index+1;
+      eachquestion.title = element['userQuestion'];
+      eachquestion.subtitle = element['explanation'];
+      eachquestion.isrequired = element['isRequired'] ? 1 : 0;
+      eachquestion.options = this.questionOptions[index];
+      eachquestion.createdBy = 0;
+      eachquestion.typeId = this.questionTypes[index];
+
+      survey.surveyQuestions.push(eachquestion);
+    });
+    return survey;
   }
 
   // drag and drop
   drop(event: CdkDragDrop<any[]>) {
     moveItemInArray(this.questions.controls, event.previousIndex, event.currentIndex);
     moveItemInArray(this.questions.value, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.questionOptions, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.questionTypes , event.previousIndex, event.currentIndex);
   }
 
   newQuestionType(): FormGroup {
     return this._formBuilder.group({
-      userQuestion: this._formBuilder.control('Your question goes here', [Validators.required]),
+      userQuestion: this._formBuilder.control('', [Validators.required]),
       explanation: this._formBuilder.control(''),
       isRequired: this._formBuilder.control(false),
-      questionType: this._formBuilder.control(''),
-      subProperties: this._formBuilder.group({
-        minValue: this._formBuilder.control('0'),
-        maxValue: this._formBuilder.control('1000')
-      }),
+      questionType: this._formBuilder.control('essay'),
+      questionOptions: this._formBuilder.control({})
     });
+  }
+
+  addQuestionOptions(data: any, i: any)
+  {
+    this.questionOptions[i] = data['options'];
+    this.questionTypes[i] = data['type'];
   }
 
   addNewQuestion() {
     this.questions.push(this.newQuestionType());
+    this.questionOptions.push({});
+    this.questionTypes.push(0);
   }
 
   removeQuestion(index: number){
     this.questions.removeAt(index);
+    this.questionOptions.splice(index, 1);
+    this.questionTypes.splice(index, 1);
   }
 
   gettypeof(data:any)
@@ -73,4 +134,63 @@ export class CreateSurveyComponent implements OnInit {
     return this.fg.get('questions') as FormArray;
   }
 
+  // open snackbar
+  openDismiss(message: string, buttontext: string) {
+    this._snackBar.open(message, buttontext, {
+      duration: 3000,
+    });
+  }
+
+  /// Get Values
+  generateLink(shareId: string): string {
+    return window.location.origin + `/poll/view/${shareId}`;
+  }
+
+  openDialog(_heading: string, _subheading: string, _message: string, _isLinkShare: boolean) {
+    const dialogRef = this.dialog.open(AlertDialogComponent, {
+      data: {
+        message: _message,
+        heading: _heading,
+        subheading: _subheading,
+        isHtml: _isLinkShare,
+        buttonText: {
+          cancel: 'Close'
+        }
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      const newSurveyDetails = this.newSurveyViewModel;
+      let sharelink = '';
+      if (newSurveyDetails != null) { sharelink = newSurveyDetails.surveyGuid; }
+      this._router.navigate([`survey/view/${sharelink}`]);
+    });
+  }
+
+}
+
+
+export class SurveyModel {
+  surveyId: number;
+  welcometitle: string;
+  welcomeimage: string;
+  emailidrequired: number;
+  endtitle: string;
+  allowduplicate: number; //default 0
+  enddate: string; //set next year date default
+  surveyQuestions: SurveyQuestionsModel[];
+  surveyGuid: string;
+}
+
+export class SurveyQuestionsModel{
+  surveyId: number;
+  surveyQuestionId: number;
+  typeId: number; // send type
+  title: string;
+  subtitle: string;
+  isrequired: number;
+  questionDisplayOrder: number; // this will be index
+  createdBy: number; // 0 for now
+  statusId: number; //0 for now
+  options: {};
 }
--- src/app/shared/survey-question/survey-question.component.ts
+++ src/app/shared/survey-question/survey-question.component.ts
@@ -13,17 +13,23 @@ export class SurveyQuestionComponent implements OnInit {
   @Input() idx: number;
   @Output() idxToRemove: EventEmitter<number> = new EventEmitter<number>();
   @Output() formChange: EventEmitter<any> = new EventEmitter();
-
+  @Output() questionAdditionalInfo: EventEmitter<any> = new EventEmitter<any>();
   public listQuestionTypes: QuestionType[];
-
+  public options = [];
+  public newitem: any;
+  public minValue: any;
+  public maxValue: any;
+  public selectedListQuestionTypes = ['essay'];
   constructor() {
   }
 
   ngOnInit() {
     this.listQuestionTypes = [
-      new QuestionType(1, 'Essay' ),
-      new QuestionType(2, 'Rating' ),
-      new QuestionType(3, 'Range' )
+      new QuestionType('essay', 'Essay'),
+      new QuestionType('radiobuttons', 'Single Option'),
+      new QuestionType('multiple', 'Multiple Options'),
+      new QuestionType('rangeslider', 'Range Slider'),
+      new QuestionType('rating', 'Rating')
     ];
     console.log(this.idx);
     console.log(this.questionForm);
@@ -31,17 +37,17 @@ export class SurveyQuestionComponent implements OnInit {
   }
 
   onChanges(): void {
-    console.log('MyForm > onChanges', this.questionForm.value);
-    this.questionForm.valueChanges.subscribe(value => {
-      this.formChange.emit(this.questionForm);
-    });
+    // console.log('MyForm > onChanges', this.questionForm.value);
+    // this.questionForm.valueChanges.subscribe(value => {
+    //   this.formChange.emit(this.questionForm);
+    // });
   }
 
-  get isRequired(){
+  get isRequired() {
     return this.questionForm.get('isRequired').value;
   }
 
-  get userQuestion(){
+  get userQuestion() {
     return this.questionForm.get('userQuestion').value;
   }
 
@@ -49,8 +55,47 @@ export class SurveyQuestionComponent implements OnInit {
     return this.listQuestionTypes;
   }
 
+  setQuestionType(idx, event) {
+    this.selectedListQuestionTypes[idx] = event.value;
+  }
+
+  SaveEmit(idx) {
+    //this.questionOptions.emit({ 'min': 10, 'max': 100 });
+
+    let emitOptions = {};
+
+    if (this.selectedListQuestionTypes[idx] == 'essay') {
+      emitOptions['min'] = this.minValue;
+      emitOptions['max'] = this.maxValue;
+    }
+
+    if (this.selectedListQuestionTypes[idx] == 'radiobuttons') {
+      let i = 0;
+      this.options.forEach(element => {
+        let valueText = 'value' + i;
+        emitOptions[valueText] = element;
+        i++;
+      });
+    }
+
+    let emitData = {
+      'options': emitOptions,
+      'type': this.listQuestionTypes.findIndex(x => x.code == this.selectedListQuestionTypes[idx])
+    };
+
+    this.questionAdditionalInfo.emit(emitData);
+    this.formChange.emit(this.questionForm);
+  }
+
+  addOption() {
+    if (this.newitem != '' && !this.options.includes(this.newitem)) {
+      this.options.push(this.newitem);
+      this.newitem = '';
+    }
+  }
+
 }
 
 export class QuestionType {
-  constructor(public id: number, public name: string) { }
+  constructor(public code: string, public name: string) { }
 }
