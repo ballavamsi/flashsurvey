ad3e061339e4d5f6af45c41b8f332ef922e1bcd1
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2020-09-21 12:39:33 +0530

    Corrected build issues

--- src/app/pages/survey/view-feedback-survey/view-feedback-survey.component.ts
+++ src/app/pages/survey/view-feedback-survey/view-feedback-survey.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { SurveyModel } from 'src/app/models/survey';
 
-@Component({
+  @Component({
     selector: 'app-view-feedback-survey',
     templateUrl: './view-feedback-survey.component.html',
     styleUrls: ['./view-feedback-survey.component.scss']
   })
+
+  export class ViewFeedbackSurveyComponent implements OnInit {
+    constructor() {
+
+    }
+    ngOnInit(){
+
+    }
+  }
