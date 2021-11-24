c87d989a474e9e99f85e11fc1900059efd930fad
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2021-11-24 15:21:28 +0530

    Update tokenInterceptor.service.ts
    added interceptor to handle session empty

--- src/app/services/tokenInterceptor.service.ts
+++ src/app/services/tokenInterceptor.service.ts
@@ -1,26 +1,44 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { StorageService } from './storage/storage.service';
 
 @Injectable()
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor(private _storageService: StorageService){}
-   intercept(req: HttpRequest<any>, next: HttpHandler):   Observable<HttpEvent<any>> {
-       // All HTTP requests are going to go through this method
+  constructor(private _storageService: StorageService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // All HTTP requests are going to go through this method
     req = this.addUserToken(req);
-    return next.handle(req);
-   }
+    return next.handle(req).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('event--->>>', event);
+        }
+        return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        let data = {};
+        data = {
+          reason: error && error.error && error.error.reason ? error.error.reason : '',
+          status: error.status
+        };
+          if (error.status == 401) {
+            this._storageService.clearAllSession();
+          return throwError(error);
+        }
+      }));
+  }
 
-   private addUserToken(request: HttpRequest<any>): HttpRequest<any> {
+  private addUserToken(request: HttpRequest<any>): HttpRequest<any> {
     var userDetails = this._storageService.getUserSessionDetails();
-    if(userDetails != null) {
-    return request.clone({
-      headers: request.headers.set("UserToken", userDetails?.userGuid)
-    });
+    if (userDetails != null) {
+      return request.clone({
+        headers: request.headers.set("UserToken", userDetails?.userGuid)
+      });
     }
-    else{
+    else {
       return request;
     }
   }
