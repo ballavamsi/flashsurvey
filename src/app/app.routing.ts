0ab7ee77c24b98fc357863abbfd2f72a3d9769d0
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2023-01-18 20:48:32 +0530

    updated to angular 11

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -40,8 +40,9 @@ const routes: Routes =[
     CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes,{
-      useHash: false
-    })
+    useHash: false,
+    relativeLinkResolution: 'legacy'
+})
   ],
   exports: [
   ],
--- src/app/layouts/prelogin-layout/prelogin-layout.component.spec.ts
+++ src/app/layouts/prelogin-layout/prelogin-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PreLoginLayoutComponent } from './prelogin-layout.component';
 
@@ -6,7 +6,7 @@ describe('PreLoginLayoutComponent', () => {
   let component: PreLoginLayoutComponent;
   let fixture: ComponentFixture<PreLoginLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PreLoginLayoutComponent ]
     })
