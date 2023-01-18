2e5b8f117659ee11441b91b340dd197d5104efdd
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2023-01-18 19:25:57 +0530

    angular 10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: "https://freepoll-api.loca.lt/api"
+  //API_URL: "https://freepoll-api.loca.lt/api"
+  API_URL: "https://flashsurvey.netlify.app/api",
 };
