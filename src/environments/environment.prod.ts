a8964fb22a8fef1e0b37c59fe3cd65d6dab74fc7
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2024-06-11 19:27:29 +0530

    Updated url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://freepoll-api.loca.lt/api',
+  // API_URL: 'https://freepoll-api.loca.lt/api',
+  API_URL: 'https://oneapigw-api.loca.lt/api',
   //API_URL: "https://flashsurvey.netlify.app/api",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  //API_URL: "http://localhost:6611/api",
+  // API_URL: "http://localhost:6611/api",
   //API_URL: "http://localhost:5000/api",
-  API_URL: "https://freepoll-api.loca.lt/api",
+  // API_URL: "https://freepoll-api.loca.lt/api",
+  API_URL: 'https://oneapigw-api.loca.lt/api',
 };
 
 /*
